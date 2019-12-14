8bed962c0cddca31322de04105dac310e2e8bbec
kengru <kengrullon@gmail.com>
2019-12-14 16:07:58 -0400

    Finally got it to crawl correctly.

--- jobs/main.js
+++ jobs/main.js
@@ -1,12 +1,22 @@
-import request from "request-promise-native";
+import rp from "request-promise";
 import cheerio from "cheerio";
 
-const fetchMangas = async () => {
-  const res = await request.get("https://jaiminisbox.com/reader/read/one-piece-2/en/0/965/page/1");
-  const $ = cheerio.load(res.response.body);
+const fetchMangas = () => {
+  const options = {
+    uri: "https://jaiminisbox.com/reader/read/one-piece-2/en/0/966/page/1",
+    transform: body => cheerio.load(body)
+  };
+  rp.get(options)
+    .then($ => $("#container > div.sorry").html())
+    .catch(err => console.log(err));
   const date = new Date();
-  console.log(`Searched for One Piece at ${date.toLocaleString("en-US")}, nothing there yet.`);
-  console.log($("#page").html());
+  console.log(
+    `Searched for One Piece at ${date.toLocaleString(
+      "en-US"
+    )}, nothing there yet.`
+  );
+  // console.log(res);
+  // console.log($("#container > div.sorry").html());
 };
 
 export default fetchMangas;
