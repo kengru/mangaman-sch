9d8eb74cc482a95548603695d28c7daf8ced9d2d
kengru <kengrullon@gmail.com>
2019-12-14 15:44:02 -0400

    Changing in order to correctly web crawl.

--- jobs/main.js
+++ jobs/main.js
@@ -1,12 +1,12 @@
-import request from "request";
+import request from "request-promise-native";
 import cheerio from "cheerio";
 
 const fetchMangas = async () => {
   const res = await request.get("https://jaiminisbox.com/reader/read/one-piece-2/en/0/965/page/1");
-  const $ = cheerio.load(res);
+  const $ = cheerio.load(res.response.body);
   const date = new Date();
   console.log(`Searched for One Piece at ${date.toLocaleString("en-US")}, nothing there yet.`);
-  console.log($);
+  console.log($("#page").html());
 };
 
 export default fetchMangas;
